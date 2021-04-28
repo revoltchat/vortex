4c90704aee8452e19681e818b7a0f76b3f572926
Martin Loffler <git@fatalerrorcoded.eu>
2021-04-28 21:19:08 +0200

    Async, logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate log;
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+    env_logger::init_from_env(env_logger::Env::default().filter_or("RUST_LOG", "info"));
+
+    info!("Starting Revolt Voso voice server");
 }
