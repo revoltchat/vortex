d24f593e2bdcba7ba2796e5e337cc88fbbcc0b3d
Martin Löffler <me@fatalerrorcoded.eu>
2021-07-06 20:01:41 +0200

    Client event loop

--- src/ws/mod.rs
+++ src/ws/mod.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use futures::{
     stream::{SplitSink, SplitStream},
     SinkExt, StreamExt,
@@ -25,6 +27,8 @@ async fn on_connection(ws: WebSocket) {
         let code = close as u16;
         let reason = close.to_string();
         ws_sink.send(Message::close_with(code, reason)).await.ok();
+    } else {
+        ws_sink.send(Message::close()).await.ok();
     }
 }
 
@@ -63,7 +67,9 @@ async fn handle(
                             },
                         };
 
-                        ws_sink.send(Message::text(serde_json::to_string(&reply)?)).await?;
+                        ws_sink
+                            .send(Message::text(serde_json::to_string(&reply)?))
+                            .await?;
                         break (room, id);
                     } else {
                         return Err(WSCloseType::InvalidState);
@@ -75,5 +81,37 @@ async fn handle(
         }
     };
 
-    Ok(())
+    // TODO: implement some sort of way to automatically remove a user from a room if the thread panics
+    // the Room user remove function is async but the Drop trait is not
+
+    let result = event_loop(&room, &user_id, ws_stream).await;
+    room.users().remove(&user_id).await.ok();
+    result
+}
+
+async fn event_loop(room: &Arc<Room>, user_id: &str, ws_stream: &mut SplitStream<WebSocket>) -> Result<(), WSCloseType> {
+    let mut room_stream = room.subscribe().ok_or(WSCloseType::RoomClosed)?;
+    let mut ws_stream = ws_stream.fuse();
+
+    loop {
+        tokio::select! {
+            message = ws_stream.next() => {
+                if let Some(message) = message {
+                    let message = message.map_err(|_| WSCloseType::ServerError)?;
+                    // Try to get the text message, ignore otherwise (might be ping, binary)
+                    if let Ok(text) = message.to_str() {
+                        let out: WSCommand = serde_json::from_str(text)?;
+                        match out {
+                            _ => todo!(),
+                        }
+                    }
+                } else {
+                    return Ok(());
+                }
+            },
+            event = room_stream.recv() => {
+                todo!();
+            }
+        }
+    }
 }
